0957b9e5072982efaf416d5708b2b4b388405dee
avicha <avichabc@gmail.com>
2019-06-13 18:44:19 +0800

    content:修改了修改密码传到字段

--- src/layouts/user/reset_password.vue
+++ src/layouts/user/reset_password.vue
@@ -2,13 +2,13 @@
   <div class="user-reset-password-page">
     <el-form class="reset-password-form" label-position="left" label-width="80px">
       <el-form-item label="旧密码">
-        <el-input type="password" name="old_password" v-model="old_password" placeholder="请输入你的旧密码" auto-complete="off"></el-input>
+        <el-input type="password" name="oldPassword" v-model="oldPassword" placeholder="请输入你的旧密码" auto-complete="off"></el-input>
       </el-form-item>
       <el-form-item label="新密码">
-        <el-input type="password" name="new_password" v-model="new_password" placeholder="请输入你的新密码" auto-complete="off"></el-input>
+        <el-input type="password" name="newPassword" v-model="newPassword" placeholder="请输入你的新密码" auto-complete="off"></el-input>
       </el-form-item>
       <el-form-item label="确认密码">
-        <el-input type="password" name="confirm_password" v-model="confirm_password" placeholder="请再次输入你的新密码" auto-complete="off"></el-input>
+        <el-input type="password" name="confirmPassword" v-model="confirmPassword" placeholder="请再次输入你的新密码" auto-complete="off"></el-input>
       </el-form-item>
       <el-form-item>
         <el-button type="primary" @click="resetPassword">重置密码</el-button>
@@ -28,28 +28,28 @@ export default {
   name: 'UserResetPassword',
   data() {
     return {
-      old_password: '',
-      new_password: '',
-      confirm_password: ''
+      oldPassword: '',
+      newPassword: '',
+      confirmPassword: ''
     }
   },
   methods: {
     resetPassword() {
-      if (!this.old_password || !this.new_password || !this.confirm_password) {
+      if (!this.oldPassword || !this.newPassword || !this.confirmPassword) {
         this.$message({
           message: '请输入旧密码、新密码、确认密码',
           type: 'error'
         })
       } else {
-        if (this.confirm_password !== this.new_password) {
+        if (this.confirmPassword !== this.newPassword) {
           this.$message({
             message: '您输入的新密码和确认密码不一致，请重新确认后输入',
             type: 'error'
           })
-          this.new_password = ''
-          this.confirm_password = ''
+          this.newPassword = ''
+          this.confirmPassword = ''
         } else {
-          resetPasswordUser({ old_password: this.old_password, new_password: this.new_password }).then(() => {
+          resetPasswordUser({ oldPassword: this.oldPassword, newPassword: this.newPassword }).then(() => {
             this.$message({
               message: '修改密码成功，请重新登录',
               type: 'success',
